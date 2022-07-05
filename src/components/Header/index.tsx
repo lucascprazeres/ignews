eddd2b80daba6ffb9f8926a8aecf09006062322a
Lucas Prazeres <olucascostadp@gmail.com>
2022-07-05 09:41:52 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image
+          src="/images/logo.svg"
+          alt="ig.news"
+          height={30} width={108}
+        />
+        <nav>
+          <a href="" className={styles.active}>Home</a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import styles from '../styles/home.module.scss'
 import Head from 'next/head'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
