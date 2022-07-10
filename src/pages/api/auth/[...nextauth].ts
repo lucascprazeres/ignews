b5607c73bedd090261207f97e2a2c087d80b5ffa
Lucas Prazeres <olucascostadp@gmail.com>
2022-07-09 23:17:30 -0300

    feat: add faunadb integration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -9,4 +13,26 @@ export default NextAuth({
       authorization: "https://github.com/login/oauth/authorize?scope=read:user",
     })
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(email!)),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email!))),
+          ),
+        );
+        return true
+      } catch {
+        return false;
+      }
+    }
+  }
 })
