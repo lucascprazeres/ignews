e87d418fd3f8abc14199cd380b73b6ddf15d112f
Lucas Prazeres <lucascprazeres@outlook.com>
2022-07-10 11:46:06 -0300

    feat: add duplicated user verification on stripe and fauna

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,31 +1,70 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from 'next-auth/react'
+import { query as q } from "faunadb";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type FaunaUser = {
+  ref: {
+    id: string;
+  },
+  data: {
+    stripe_costumer_id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req })
 
-    if (session && session.user) {
+    const user = await fauna.query<FaunaUser>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session?.user?.email!)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_costumer_id
+
+    if (!customerId) {
+
       const stripeCostumer = await stripe.customers.create({
-        email: session.user.email!,
+        email: session?.user?.email!,
       })
 
-      const stripeCheckoutSession = await stripe.checkout.sessions.create({
-        customer: stripeCostumer.id,
-        payment_method_types: ['card'],
-        billing_address_collection: 'required',
-        line_items: [
-          { price: 'price_1JMxu1GKTtrj98uqcQPoKz6I', quantity: 1 }
-        ],
-        mode: 'subscription',
-        allow_promotion_codes: true,
-        success_url: process.env.STRIPE_SUCCESS_URL!,
-        cancel_url: process.env.STRIPE_CANCEL_URL!
-      })
+      await fauna.query(
+        q.Update(
+          q.Ref(
+            q.Collection('users'),
+            user.ref.id
+          ),
+          {
+            data: {
+              stripe_costumer_id: stripeCostumer.id
+            }
+          }
+        )
+      )
 
-      return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+      customerId = stripeCostumer.id 
     }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1JMxu1GKTtrj98uqcQPoKz6I', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL!,
+      cancel_url: process.env.STRIPE_CANCEL_URL!
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
